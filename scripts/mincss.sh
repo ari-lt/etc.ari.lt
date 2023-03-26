7452983f50b5d4e9b1f6ed2efb015de9f5aab787
Ari Archer <ari.web.xyz@gmail.com>
2023-03-26 17:04:26 +0300

    properly minify css
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/mincss.sh
+++ scripts/mincss.sh
@@ -9,7 +9,7 @@ main() {
         [ -d 'content/styles/' ] || return
 
         find content/styles/ -not -ipath "./node_modules/*" -type f \
-            -name "*.js" ! -name "*.min.*" ! -name "vfs_fonts*" \
+            -name "*.css" ! -name "*.min.*" ! -name "vfs_fonts*" \
             -exec uglifycss --output {}.min {} \; \
             -exec rm {} \; \
             -exec mv {}.min {} \;
